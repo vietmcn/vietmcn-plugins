31818ef485e2aba43b6b95a5e87a03197546e866
vietmcn <vietmcn.com@gmail.com>
2017-11-25 21:02:09 +0700

    Update

--- App/Models/Product-filter/class.filter-price.php
+++ App/Models/Product-filter/class.filter-price.php
@@ -5,8 +5,5 @@ class Vietmcn_filter_price_models extends Vietmcn_models
     {
         add_action( 'wp_footer', array( $this, 'price_min' ) );
     }
-    public function price_min() 
-    {
-        echo $_GET['price_min'];
-    }
+    
 }
